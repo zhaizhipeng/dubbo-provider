01c5c4e06149ccd45b73f940176d4209e474d85b
zhaizhipeng <zhaizhipeng@extracme.com>
2020-11-05 10:39:01 +0800

    优化代码

---
+++ dubbo-provider-service/src/main/java/com/ysdrzp/provider/App.java
@@ -0,0 +1,15 @@
+package com.ysdrzp.provider;
+
+import com.alibaba.dubbo.container.Main;
+
+/**
+ * 生产者启动类
+ * @author 向往的生活
+ */
+public class App {
+
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+
+}
